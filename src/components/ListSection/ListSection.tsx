9c2fc4436522a9f7a2677a5df16df8c5b960f83c
Leticia <leticia.schneider@gmail.com>
2025-01-31 18:09:01 -0300

    initial files

---
+++ src/components/ListSection/ListSection.tsx
@@ -0,0 +1,7 @@
+function ListSection() {
+    return (
+        <div>Add ListSection</div>
+    )
+}
+
+export default ListSection
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,7 @@
+function Sidebar() {
+    return (
+        <div>Add Sidebar Content</div>
+    )
+}
+
+export default Sidebar
