b78af7a1d95b581aeb26c438c1a50ab7c2e41311
Leticia <leticia.schneider@gmail.com>
2025-02-17 00:10:59 -0300

    add enter key support and reset on cancel

--- src/components/Sidebar/NewCategoryForm.tsx
+++ src/components/Sidebar/NewCategoryForm.tsx
@@ -30,6 +30,11 @@ function NewCategoryForm({ onAddCategory }: NewCategoryFormProps) {
             onChange={(e) => setNewCategory(e.target.value)}
             placeholder="New category"
             className={classes.newListInput}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleAddCategory();
+              }
+            }}
             autoFocus
           />
           <button className={classes.confirmButton} onClick={handleAddCategory}>
@@ -38,7 +43,10 @@ function NewCategoryForm({ onAddCategory }: NewCategoryFormProps) {
             </svg>
           </button>
 
-          <button className={classes.cancelButton} onClick={() => setIsAdding(false)}>
+          <button className={classes.cancelButton} onClick={() => {
+            setIsAdding(false);
+            setNewCategory("");
+          }}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <path d="M18 6 6 18" />
               <path d="m6 6 12 12" />
